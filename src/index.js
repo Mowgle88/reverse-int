1fe998fa97013b2c9ada8b3a7900d485fe1f9d74
Mowgle88 <mowgle1988@gmail.com>
2021-08-15 17:00:50 +0300

    comlited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = Math.abs(n).toString();
+	let rev = "";
+	for(let i = 0; i < result.length; i++) {
+		rev = result[i] + rev;
+	}
+
+	return +rev
 }
